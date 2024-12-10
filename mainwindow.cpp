9a58f7af4ac9b5331c7bc69c3b708d0bb5850dad
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-12-11 00:53:59 +0700

    Add functions for changing pages

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,13 +13,22 @@ MainWindow::MainWindow(QWidget *parent)
     // Подключение сигналов и слотов
     connect(ui->icon_menu_toggle_button, &QPushButton::clicked, this, &MainWindow::showFullMenu);
     connect(ui->full_menu_toggle_button, &QPushButton::clicked, this, &MainWindow::showIconMenu);
+
+    // Подключаем сигналы кнопок к слоту переключения страниц
+    connect(ui->icon_menu_today_button, &QPushButton::clicked, this, &MainWindow::showTodayPage);
+    connect(ui->full_menu_today_button, &QPushButton::clicked, this, &MainWindow::showTodayPage);
+
+    connect(ui->icon_menu_calendar_button, &QPushButton::clicked, this, &MainWindow::showCalendarPage);
+    connect(ui->full_menu_calendar_button, &QPushButton::clicked, this, &MainWindow::showCalendarPage);
+
+    connect(ui->icon_menu_tasks_button, &QPushButton::clicked, this, &MainWindow::showTasksPage);
+    connect(ui->full_menu_tasks_button, &QPushButton::clicked, this, &MainWindow::showTasksPage);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
 
 void MainWindow::showFullMenu() {
     ui->full_menu_widget->show();
@@ -29,4 +38,17 @@ void MainWindow::showFullMenu() {
 void MainWindow::showIconMenu() {
     ui->full_menu_widget->hide();
     ui->icon_menu_widget->show();
+}
+
+// Эти слоты переключают страницы в QStackedWidget
+void MainWindow::showTodayPage() {
+    ui->stackedWidget->setCurrentWidget(ui->today_page);  // Переходим на страницу today_page
+}
+
+void MainWindow::showCalendarPage() {
+    ui->stackedWidget->setCurrentWidget(ui->calendar_page);  // Переходим на страницу calendar_page
+}
+
+void MainWindow::showTasksPage() {
+    ui->stackedWidget->setCurrentWidget(ui->tasks_page);  // Переходим на страницу tasks_page
 }
--- mainwindow.h
+++ mainwindow.h
@@ -21,7 +21,10 @@ private:
     Ui::MainWindow *ui;
 
 private slots:
-    void showFullMenu();    // Слот для отображения full_menu_widget
-    void showIconMenu();    // Слот для отображения icom_menu_widget
+    void showFullMenu();        // Слот для отображения full_menu_widget
+    void showIconMenu();        // Слот для отображения icom_menu_widget
+    void showTodayPage();       // Слот для преключения на страницу today_page
+    void showCalendarPage();    // Слот для преключения на страницу calendar_page
+    void showTasksPage();       // Слот для преключения на страницу tasks_page
 };
 #endif // MAINWINDOW_H
